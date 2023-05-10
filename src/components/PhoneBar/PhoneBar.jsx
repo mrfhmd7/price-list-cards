a0a85ff76d916403e343cca6dc1a94c011510123
mrfhmd7 <mrf.hmd7@gmail.com>
2023-05-11 02:23:47 +0600

    finished

--- src/components/PhoneBar/PhoneBar.jsx
+++ src/components/PhoneBar/PhoneBar.jsx
@@ -24,7 +24,7 @@ const PhoneBar = () => {
                          }
                          return phoneInfo;
                     })
-                    console.log(phonesData);
+                    // console.log(phonesData);
                     setPhones(phonesData);
                })
      }, [])
